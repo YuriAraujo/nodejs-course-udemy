3977a3dc7981326748d32398e524d2e923268297
Yuri Araújo <yuri.araujo@eztravel.com.br>
2018-11-20 18:47:09 -0300

    class 115 - Geolocation Part II

--- node-chat-app/public/js/index.js
+++ node-chat-app/public/js/index.js
@@ -10,4 +10,46 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
 	console.log('newMessage', message);
+	let li = jQuery('<li></li>');
+	li.text(`${message.from}: ${message.text}`);
+
+	jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (message) {
+	let li = jQuery('<li></li>');
+	let a = jQuery('<a target="_blank">My Location</a>');
+
+	li.text(`${message.from}: `);
+	a.attr('href', message.url);
+	li.append(a);
+	jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+	e.preventDefault();
+
+	socket.emit('createMessage', {
+		from: 'User',
+		text: jQuery('[name=message]').val()
+	}, function () {
+
+	});
+});
+
+let locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+	if (!navigator.geolocation) {
+		return alert('Geolocation not supported by your browser.');
+	}
+
+	navigator.geolocation.getCurrentPosition(function (position) {
+		socket.emit('createLocationMessage', {
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+		console.log(position);
+	}, function () {
+		alert('Unable to fetch location.')
+	});
 });
--- node-chat-app/server/utils/message.test.js
+++ node-chat-app/server/utils/message.test.js
@@ -1,6 +1,6 @@
 let expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
 		expect(res.createdAt).toBeA('number');
 		expect(res).toInclude({from, text});
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		let from = 'Yuri';
+		let latitude = 15;
+		let longitude = 19;
+		let url = 'https://maps.google.com/?q=15,19';
+		let message = generateLocationMessage(from, latitude, longitude);
+
+		expect(message.createdAt).toBeA('number');
+		expect(message).toInclude({from, url});
 	});
 });
